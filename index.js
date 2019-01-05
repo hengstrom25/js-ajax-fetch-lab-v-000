4c8aacb42b4fff20f0dcd7fda119ef7046326cfd
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-05 01:11:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
-}
+  
+  const token = '';
+ 
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+   .then(res => res.json())
+    .then(json => console.log(json));
+    return '';
+  }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
